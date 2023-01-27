5fcedd2c9300351c2ed21e6fbd5fb5305d724877
Minu Mathew <minumpmathews@gmail.com>
2023-01-27 14:04:49 -0600

    updated dashboard for explore dataset

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,7 +3,7 @@ import {Link as RouterLink} from "react-router-dom";
 import {AppBar, Box, Button, Dialog, DialogTitle, Grid, Link, ListItem, Tab, Tabs, Typography} from "@material-ui/core";
 import TopBar from "./childComponents/TopBar";
 import GetDataset from "./childComponents/GetDataset";
-import CreateDataset from "./childComponents/CreateDataset";
+//import CreateDataset from "./childComponents/CreateDataset";
 
 export default function Dashboard() {
 	return (
@@ -12,11 +12,11 @@ export default function Dashboard() {
 			<div className="outer-container">
 				<div className="inner-container">
 					<Grid container direction="row" item md={8} xs={12} style={{ display: "flex", gap: "1rem", justifyContent:"flex-start" }}>
-						{/*<Grid item>*/}
-						{/*	<Box className="getDataset">*/}
-						{/*		<GetDataset />*/}
-						{/*	</Box>*/}
-						{/*</Grid>*/}
+						<Grid item>
+							<Box className="getDataset">
+								<GetDataset />
+							</Box>
+						</Grid>
 						{/*<Grid item>*/}
 						{/*	<Box className="createDataset">*/}
 						{/*		<CreateDataset />*/}
