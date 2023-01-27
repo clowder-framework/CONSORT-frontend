1bb51a83d4cf7f30a8258e4aa9264b319a5081ea
Minu Mathew <minumpmathews@gmail.com>
2023-01-27 13:24:20 -0600

    added routes

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import Dashboard from "./components/Dashboard";
+
+export const AppRoutes = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Dashboard/>}/>
+				<Route path="*"
+					   element={
+						   <main style={{padding: "1rem"}}>
+							   <p>Page Not Found!</p>
+						   </main>
+					   }
+				/>
+			</Routes>
+		</BrowserRouter>
+	)
+}
