fc1a951a0dbcc834934c2246f0dd929506d06c36
Minu Mathew <minumpmathews@gmail.com>
2023-02-10 16:50:20 -0600

    added previews

--- src/components/childComponents/CreateAndUpload.js
+++ src/components/childComponents/CreateAndUpload.js
@@ -13,6 +13,10 @@ import Dropfile from "./Dropfile";
 import Html from "../previewers/Html";
 import config from "../../app.config";
 import {getHeader} from "../../utils/common";
+import {downloadResource} from "../../utils/common";
+import Audio from "../previewers/Audio";
+import Video from "../previewers/Video";
+import Thumbnail from "../previewers/Thumbnail";
 
 async function createDatasetRequest(body_data) {
 	const create_dataset_url = `${config.hostname}/clowder/api/datasets/createempty?superAdmin=true`;
@@ -106,15 +110,15 @@ async function checkExtractionStatus(file){
 }
 
 async function checkHtmlInDataset(dataset){
-	// function to check if an html file is there in the dataset
+	// function to check if html file is there in the dataset
 	const dataset_id = dataset["id"];
 	const listFiles_url = `${config.hostname}/clowder/api/datasets/${dataset_id}/listFiles`;
 	// get the list of files in dataset
-	const dataset_listFiles_response = await fetch(listFiles_url, {method:"GET", headers:getHeader()});
+	const dataset_listFiles_response = await fetch(listFiles_url, {method:"GET", headers:getHeader(), mode: "cors"});
 	const dataset_listFiles = await dataset_listFiles_response.json();
+	// filter html file and select the first item in filtered array.
+	const htmlFile = Object.values(dataset_listFiles).filter(file => file.contentType === "text/html")[0];
 	// [ {"id":string, "size":string, "date-created":string, "contentType":text/html, "filename":string} ]
-	const htmlFile = Object.values(dataset_listFiles).filter(file => file.contentType === "text/html");
-	console.log(htmlFile);
 	if (htmlFile !== undefined && htmlFile.contentType === "text/html") {
 		// found html file in dataset. return the object
 		console.log("html file generated");
@@ -127,21 +131,21 @@ async function checkHtmlInDataset(dataset){
 
 }
 
-
 async function getPreviewUrl(file_id) {
-	const previews_url = `${config.hostname}/clowder/api/files/${file_id}/getPreviews`;
-	let response = await fetch(previews_url, {method:"GET", headers:getHeader()});
-	if (response.status === 200) {
-		let json_response = response.json();
-		json_response[0]["previews"].map((preview) => {
-			if(preview["pv_contenttype"] === "text/html") {
-				return preview["pv_route"];
-			}
-			else{
-				console.log("preview error");
-				return null;
-			}
-		});
+	const previews_url = `${config.hostname}/clowder/api/files/${file_id}/getPreviews?superAdmin=true`;
+	const previews_response = await fetch(previews_url, {method:"GET", mode: "cors", headers:getHeader()});
+	// [{"file_id":"63e6a5dfe4b034120ec4f035","previews":[{"pv_route":"/clowder/files/63e6a5dfe4b034120ec4f035/blob","p_main":"html-iframe.js","pv_id":"63e6a5dfe4b034120ec4f035","p_path":"/clowder/assets/javascripts/previewers/html","p_id":"HTML","pv_length":"21348","pv_contenttype":"text/html"}]}]
+	let previews_list = [];
+	if (previews_response.status === 200) {
+		const file_preview = await previews_response.json();
+		if (file_preview[0].file_id !== undefined){
+			file_preview[0].previews.map((preview) => previews_list.push(preview));
+			console.log("preview generated");
+		}
+		return previews_list;
+	}
+	else{
+		console.log("preview failed");
 	}
 }
 
@@ -150,6 +154,7 @@ export default function CreateAndUpload() {
 	const [clowderDataset, setClowderDataset] = useState(null); // state for created dataset in Clowder
 	const [clowderFile, setClowderFile] = useState(null);  // state for uploaded file in Clowder
 	const [extractionJob, setExtractionJob] = useState(null);  // state for extraction job ID and status
+	const [previews, setPreviews] = useState([]); // state for file previews
 
 	// if dropFile state has changed, create and upload to dataset
 	useEffect(async () => {
@@ -173,7 +178,7 @@ export default function CreateAndUpload() {
 		if (clowderFile !== null) {
 			const body = {"extractor": "ncsa.rctTransparencyExtractor"};
 			//const extractor_name = "ncsa.wordcount";
-			await extractionsRequest(clowderFile, body).then((response)=> {setExtractionJob(response)}); // end of extraction request
+			await extractionsRequest(clowderFile, body).then((response)=> setExtractionJob(response)); // end of extraction request
 		}
 	}, [clowderFile]);
 
@@ -183,14 +188,37 @@ export default function CreateAndUpload() {
 			const extraction_status = await checkExtractionStatus(clowderFile);
 			console.log(extraction_status);
 			if (extraction_status["Status"] === "Done" && extraction_status["ncsa.rctTransparencyExtractor"] === "DONE") {
-				const htmlFile = checkHtmlInDataset(clowderDataset);
-				if (htmlFile !== undefined && htmlFile.contentType === "text/html") {
+				const htmlFile = await checkHtmlInDataset(clowderDataset);
+				console.log(htmlFile);
+				if (typeof htmlFile.id === "string") {
 					// {"id":string, "size":string, "date-created":string, "contentType":text/html, "filename":string}
-					const preview_url = await getPreviewUrl(htmlFile.id);
-					if (preview_url !== null) {
-						const htmlFileUrl = `${config.hostname}/${preview_url}`;
-						console.log(htmlFileUrl);
-						return <Html fileId={htmlFile["id"]} htmlSrc={htmlFileUrl}/>;
+					const previews_list = await getPreviewUrl(htmlFile.id);
+					const preview = previews_list[0];
+					console.log(preview);
+					if (preview !== undefined) {
+						let previewsTemp = [];
+						// get all preview resources
+						let preview_config = {};
+						preview_config.previewType = preview["p_id"].replace(" ", "-").toLowerCase(); // html
+						preview_config.url = `${config.hostname}${preview["pv_route"]}?superAdmin=true`;
+						preview_config.fileid = preview["pv_id"];
+						preview_config.previewer = `/public${preview["p_path"]}/`;
+						preview_config.fileType = preview["pv_contenttype"];
+
+						// TODO need to fix on clowder v1: sometimes pv_route return the non-API routes
+						// /clowder/file vs clowder/api/file
+						// TODO Temp fix insert /api/
+						let pv_routes = preview["pv_route"];
+						if (!pv_routes.includes("/api/")) {
+							pv_routes = `${pv_routes.slice(0, 9)}api/${pv_routes.slice(9, pv_routes.length)}`;
+						}
+						const resourceURL = `${config.hostname}${pv_routes}?superAdmin=true`;
+						preview_config.resource = await downloadResource(resourceURL);
+						previewsTemp.push(preview_config);
+						setPreviews(previewsTemp); // set previews
+					}
+					else{
+						console.log("preview generation failed");
 					}
 
 				} else {
@@ -229,9 +257,41 @@ export default function CreateAndUpload() {
 					<FormControlLabel value="spirit" control={<Radio />} label="SPIRIT" />
 				</RadioGroup>
 			</FormControl>
+
+			<div className="previewBox">
+				{
+					previews.map((preview) => {
+						if (preview["previewType"] === "audio") {
+							return (
+								<div key={preview["fileid"]}>
+									<Audio fileId={preview["fileid"]} audioSrc={preview["resource"]}/>;
+								</div>
+							);
+						} else if (preview["previewType"] === "video") {
+							return (
+								<div key={preview["fileid"]}>
+									<Video fileId={preview["fileid"]} videoSrc={preview["resource"]}/>;
+								</div>
+							);
+						} else if (preview["previewType"] === "thumbnail") {
+							return (
+								<div key={preview["fileid"]}>
+									<Thumbnail fileId={preview["fileid"]} fileType={preview["fileType"]}
+											   imgSrc={preview["resource"]}/>;
+								</div>
+							);
+						} else if (preview["previewType"] === "html") {
+							return (
+								<div key={preview["fileid"]}>
+									<Html fileId={preview["fileid"]} htmlSrc={preview["resource"]}/>;
+								</div>
+							);
+						}
+
+					})
+				}
+			</div>
 		</Box>
-
-
 
 	);
 
