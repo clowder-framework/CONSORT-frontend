b700aba222759bbde16bec6f338f56b5403c0041
Minu Mathew <minumpmathews@gmail.com>
2023-03-06 12:32:45 -0600

    added filepreviews functionality

--- src/components/childComponents/CreateAndUpload.js
+++ src/components/childComponents/CreateAndUpload.js
@@ -1,5 +1,4 @@
-// Create a dataset and upload a file to it
-// Use Clowder createempty dataset API to create an empty dataset and uploadToDataset API to upload file to that dataset
+// Create a dataset and upload a file and submit for extraction
 
 import React, {useEffect, useState, useCallback} from 'react';
 import {useDispatch, useSelector} from "react-redux";
@@ -13,15 +12,12 @@ import FormLabel from '@mui/material/FormLabel';
 
 import Dropfile from "./Dropfile";
 import Html from "../previewers/Html";
-import config from "../../app.config";
-import {getHeader} from "../../utils/common";
-import {downloadResource} from "../../utils/common";
 import Audio from "../previewers/Audio";
 import Video from "../previewers/Video";
 import Thumbnail from "../previewers/Thumbnail";
 import {createEmptyDataset as createEmptyDatasetAction, createUploadExtract} from "../../actions/dataset";
 import {uploadFileToDataset as uploadFileToDatasetAction} from "../../actions/dataset";
-import {checkExtractionStatus, getPreviewsRequest} from "../../utils/file";
+import {checkExtractionStatus, getPreviewResources} from "../../utils/file";
 import {checkHtmlInDatasetRequest} from "../../utils/dataset";
 import {fetchFilePreviews} from "../../actions/file";
 
@@ -30,66 +26,40 @@ export default function CreateAndUpload() {
 	const dispatch = useDispatch();
 
 	const [mouseHover, setMouseHover] = useState(false); // mouse hover state for dropzone
-	const [loading, setLoading] = useState(false); // processing state. set to active when dropfile is active
-	const [extractionJob, setExtractionJob] = useState(null);  // state for extraction job ID and status
+	const [loading, setLoading] = useState(false); // loading overlay state. set to active when dropfile is active
+	const [loading_text , setLoadingText] = useState("processing"); // loading overlay text.
 	const [previews, setPreviews] = useState([]); // state for file previews
-	const listFilePreviews = (fileId) => dispatch((fetchFilePreviews(fileId)));
+
+	const listFilePreviews = (fileId) => dispatch(fetchFilePreviews(fileId));
 
 	const datasets = useSelector((state) => state.dataset.datasets);
 	const filesInDataset = useSelector(state => state.dataset.files);
+	const filePreviews = useSelector((state) => state.file.previews);
+
 	const extractor_name = "ncsa.rctTransparencyExtractor"
 
-	function selectDatasetID(){
-		// not able to useSelector here
-		//const datasets = useSelector((state) => state.dataset.datasets);
-		return datasets.map(dataset => dataset.id);
-	}
 
 	const onDropFile = (file) => {
+		setLoadingText("Uploading file");
 		dispatch(createUploadExtract(file, extractor_name));
 	};
 
+	// useEffect on filesInDataset for preview generation
 	useEffect(async()=> {
 		const file_id = filesInDataset[0].id;
 		const dataset_id = datasets[0].id;
+		// check extraction status and html file generation in loop
 		const loop = async () => {
+			setLoadingText("Checking extraction status");
 			const extraction_status = await checkExtractionStatus(file_id);
 			console.log(extraction_status);
 			if (extraction_status["Status"] === "Done" && extraction_status[extractor_name] === "DONE") {
+				setLoadingText("Generating html file");
 				const htmlFile = await checkHtmlInDatasetRequest(dataset_id);
 				console.log(htmlFile);
 				if (typeof htmlFile.id === "string") {
 					// {"id":string, "size":string, "date-created":string, "contentType":text/html, "filename":string}
-					const previews_list = await getPreviewsRequest(htmlFile.id);
-					const preview = previews_list[0];
-					console.log(preview);
-					if (preview !== undefined) {
-						let previewsTemp = [];
-						// get all preview resources
-						let preview_config = {};
-						preview_config.previewType = preview["p_id"].replace(" ", "-").toLowerCase(); // html
-						preview_config.url = `${config.hostname}${preview["pv_route"]}?superAdmin=true`;
-						preview_config.fileid = preview["pv_id"];
-						preview_config.previewer = `/public${preview["p_path"]}/`;
-						preview_config.fileType = preview["pv_contenttype"];
-
-						// TODO need to fix on clowder v1: sometimes pv_route return the non-API routes
-						// /clowder/file vs clowder/api/file
-						// TODO Temp fix insert /api/
-						let pv_routes = preview["pv_route"];
-						if (!pv_routes.includes("/api/")) {
-							pv_routes = `${pv_routes.slice(0, 9)}api/${pv_routes.slice(9, pv_routes.length)}`;
-						}
-						const resourceURL = `${config.hostname}${pv_routes}?superAdmin=true`;
-						preview_config.resource = await downloadResource(resourceURL);
-						previewsTemp.push(preview_config);
-						setPreviews(previewsTemp); // set previews
-						setLoading(false); // stop display of Overlay
-					}
-					else{
-						console.log("preview generation failed");
-					}
-
+					listFilePreviews(htmlFile.id);
 				} else {
 					console.log("check html file after 5s");
 					setTimeout(loop, 5000);
@@ -98,14 +68,34 @@ export default function CreateAndUpload() {
 				console.log("check extraction status after 5s");
 				setTimeout(loop, 5000);
 			}
-
 		};
-		if (extractionJob !== null){
+
+		if (file_id !== null){
 			await loop(); // call the loop to check extractions
 		}
+		else{
+			console.log("file does not exist");
+		}
 	}, [filesInDataset]);
+	// TODO how to make this dependency better? Now filesInDataset.id throws an error on start
 
-	// onDrop function
+
+	// useEffect on filePreviews to download preview resources
+	useEffect( async ()=> {
+		if (filePreviews !== undefined && filePreviews.length > 0) {
+			const previewsTemp = [];
+			filePreviews[0].map(async (preview) => {
+				// get all preview resources
+				const preview_config = await getPreviewResources(preview);
+				previewsTemp.push(preview_config);
+				setPreviews(previewsTemp); // set previews
+				setLoading(false); // stop display of Overlay
+			});
+		}
+	}, [filePreviews])
+
+
+	// onDrop function to trigger createUploadExtract action dispatch
 	const onDrop = useCallback(acceptedFiles => {
 		// this callback will be called after files get dropped, we will get the acceptedFiles. If you want, you can even access the rejected files too
 		acceptedFiles.map(file => onDropFile(file));
@@ -119,7 +109,7 @@ export default function CreateAndUpload() {
 			<LoadingOverlay
 				active={loading}
 				spinner
-				text="Processing..."
+				text={loading_text}
 			>
 			<div className="mousehoverdrop" onMouseEnter={()=> setMouseHover(true)} >
 				<Dropfile onDrop={onDrop}
