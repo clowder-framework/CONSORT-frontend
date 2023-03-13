129e13119dad8c971ae54b66bec995e97ace134b
Minu Mathew <minumpmathews@gmail.com>
2023-03-13 12:16:45 -0500

    added preview route and component

---
+++ src/components/Preview.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState, Component} from "react";
+import {AppBar, Box, Button, Dialog, DialogTitle, Grid, Link, ListItem, Tab, Tabs, Typography} from "@material-ui/core";
+import TopBar from "./childComponents/TopBar";
+import FilePreview from "./childComponents/FilePreview";
+
+function Preview() {
+	return (
+		<>
+			<TopBar/>
+			<div className="outer-container">
+				<div className="inner-container">
+					<Grid container spacing={2} direction="row" style={{ display: "flex", gap: "2rem", justifyContent:"flex-start" }}>
+						<Grid item xs={10} >
+							<Box className="filePreview">
+								<FilePreview />
+							</Box>
+						</Grid>
+					</Grid>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Preview;
