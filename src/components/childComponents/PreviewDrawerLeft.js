775870f2e76ba12e091e9eab8eafbee8437a75dd
Minu Mathew <minumpmathews@gmail.com>
2023-07-11 15:50:41 -0500

    added metadata in PreviewDrawer test

--- src/components/childComponents/PreviewDrawerLeft.js
+++ src/components/childComponents/PreviewDrawerLeft.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Box, Button} from "@material-ui/core";
+import {Box, Button, Typography} from "@material-ui/core";
 import Drawer from '@mui/material/Drawer';
 import Toolbar from '@mui/material/Toolbar';
 import Divider from '@mui/material/Divider';
@@ -9,10 +9,14 @@ import {downloadFile} from "../../utils/file";
 const drawerWidth = 240;
 
 export default function PreviewDrawerLeft(props) {
-	const {fileId, fileSrc, ...other} = props;
+	const {fileId, fileSrc, metadata, ...other} = props;
+	console.log(metadata);
+	const extractor = metadata["extractor"];
+	const items_missed = metadata["items_missed"];
+	const checklist = metadata["checklist"];
 
 	const onDownload = () => {
-		downloadFile(fileId, "results.html");
+		downloadFile(fileId, "results.html").then(r => console.log(r));
 	}
 
 	return (
@@ -31,6 +35,10 @@ export default function PreviewDrawerLeft(props) {
 				anchor="left"
 			>
 				<Toolbar>
+					<Box variant="contained" color="primary-light">
+						<Typography>Items Missed</Typography>
+						<Typography>${items_missed}</Typography>
+					</Box>
 					<Button onClick={onDownload} variant="contained" color="primary" startIcon={<DownloadIcon />}>
 						Export
 					</Button>
