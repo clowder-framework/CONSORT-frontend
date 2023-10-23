6320bbe50272627354e5e5e59c6c374a3a1b5bc9
Minu Mathew <minumpmathews@gmail.com>
2023-10-23 16:20:37 -0500

    set correct metadata for rct and pdf extractions

--- src/actions/client.js
+++ src/actions/client.js
@@ -1,12 +1,19 @@
 // client actions
 
-import {createEmptyDatasetRequest, getFileInDataset, uploadFileToDatasetRequest} from "../utils/dataset";
+import {
+	createEmptyDatasetRequest,
+	getDatasetMetadata,
+	getFileInDataset,
+	uploadFileToDatasetRequest
+} from "../utils/dataset";
 import {checkExtractionStatusLoop, submitForExtraction} from "../utils/file";
 import config from "../app.config";
 import {getClientInfo} from "../utils/common";
 import {SET_EXTRACTION_STATUS, setExtractionStatus} from "./file";
 import {ADD_FILE_TO_DATASET, addFileToDataset, CREATE_DATASETS, createDataset} from "./dataset";
 
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 // createUploadExtract thunk function
 export function createUploadExtract(file) {
@@ -49,9 +56,8 @@ export function createUploadExtract(file) {
 				else if (file.type == "application/pdf") {
 					const pdf_extraction_submission = await submitForExtraction(file_json.id, config.pdf_extractor, clientInfo);
 					if (pdf_extraction_submission) {
-						//const pdf_extraction_status = await checkExtractionStatusLoop(file_json.id, config.pdf_extractor, 5000, clientInfo);
-						const pdf_extraction_status = true;
-						if (pdf_extraction_status === true){
+						const pdf_metadata = await getPdfMetadataLoop(dataset_json.id, clientInfo);
+						if (pdf_metadata !== null){
 							console.log("pdf extraction done");
 							const text_file_name = file_name + '.txt';
 							const extracted_txt_file = await getFileInDataset(dataset_json.id, "text/file", text_file_name, clientInfo);
@@ -102,4 +108,28 @@ export function createUploadExtract(file) {
 			}
 		}
 	};
+}
+
+async function getPdfMetadataLoop(dataset_id, clientInfo){
+	// method to check if metadata has been uploaded to dataset. else wait in loop
+	// check pdf2text extraction status and dataset metadata generation in loop
+	const pdf_metadata_loop = async () => {
+		// get dataset metadata as json-ld
+		const metadata = await getDatasetMetadata(dataset_id, clientInfo);
+		if (metadata !== null && metadata.length > 0) {
+			return metadata[0]["content"];
+		} else {
+			console.log("check pdf extraction after 30s");
+			await sleep(30000);
+			await pdf_metadata_loop();
+			//setTimeout(pdf_metadata_loop, 30000);
+		}
+	};
+
+	if (dataset_id !== null) {
+		await pdf_metadata_loop(); // call the loop to check extractions
+	} else {
+		console.error("Dataset does not exist");
+		return [];
+	}
 }
