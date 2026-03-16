8386904c2806843f1b68720926909c99ed15d8ec
Minu Mathew <minumpmathews@gmail.com>
2026-03-16 17:19:50 -0500

    remove unsued auth and local db

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,56 +3,7 @@ const fetch = require("node-fetch");
 var express = require('express');
 var passport = require('passport');
 const OAuth2Strategy = require('passport-oauth2').Strategy;
-var db = require('../sessiondb');
 
-
-
-// Configure the Google strategy for use by Passport.
-//
-// OAuth 2.0-based strategies require a `verify` function which receives the
-// credential (`accessToken`) for accessing the Facebook API on the user's
-// behalf, along with the user's profile.  The function must invoke `cb`
-// with a user object, which will be set at `req.user` in route handlers after
-// authentication.
-// passport.use(new GoogleStrategy({
-//   clientID: process.env['GOOGLE_CLIENT_ID'],
-//   clientSecret: process.env['GOOGLE_CLIENT_SECRET'],
-//   callbackURL: '/oauth2/redirect/google',
-//   scope: [ 'profile' ]
-// }, function verify(issuer, profile, cb) {
-//   db.get('SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?', [
-//     issuer,
-//     profile.id
-//   ], function(err, row) {
-//     if (err) { return cb(err); }
-//     if (!row) {
-//       db.run('INSERT INTO users (name) VALUES (?)', [
-//         profile.displayName
-//       ], function(err) {
-//         if (err) { return cb(err); }
-//         var id = this.lastID;
-//         db.run('INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)', [
-//           id,
-//           issuer,
-//           profile.id
-//         ], function(err) {
-//           if (err) { return cb(err); }
-//           var user = {
-//             id: id,
-//             name: profile.displayName
-//           };
-//           return cb(null, user);
-//         });
-//       });
-//     } else {
-//       db.get('SELECT * FROM users WHERE id = ?', [ row.user_id ], function(err, row) {
-//         if (err) { return cb(err); }
-//         if (!row) { return cb(null, false); }
-//         return cb(null, row);
-//       });
-//     }
-//   });
-// }));
 
 const CIlogon_idp = [
 	{"EntityID":"https://idp.ncsa.illinois.edu/idp/shibboleth","OrganizationName":"National Center for Supercomputing Applications","DisplayName":"National Center for Supercomputing Applications","RandS":true},
@@ -64,6 +15,7 @@ const CIlogon_idp = [
 const encodedEntityIDs = CIlogon_idp.map(item => encodeURIComponent(item.EntityID));
 const concatenatedEntityIDs = encodedEntityIDs.join(',');
 
+// Not used currently. Can be used to fill organization info for Users table in RCTDB.
 function lookupIdpMeta(entityId) {
   if (!entityId) return { organizationName: null, idpDisplayName: null };
   const found = CIlogon_idp.find((item) => item.EntityID === entityId);
